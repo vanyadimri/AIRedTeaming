9821219ea31b396a4483bbfee3da2507df3e9314
Vanya Dimri <vanyadimri@Vanyas-MacBook-Pro.local>
2023-05-10 19:59:41 -0700

    test prompt

--- main_code.py
+++ main_code.py
@@ -4,4 +4,9 @@ import openai
 with open(file= "../key.txt") as f:
     openai.api_key = f.read()
 
-openai.Model.list()
+openai.Completion.create(
+  model="text-davinci-003",
+  prompt="Say this is a test",
+  max_tokens=7,
+  temperature=0
+)
