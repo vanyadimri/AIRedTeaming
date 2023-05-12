7af2cea643b130ef6293d14fc190d36968a6d6a9
Sydney <smkuhl@uw.edu>
2023-05-12 15:59:25 -0700

    chat completion

--- main_code.py
+++ main_code.py
@@ -4,9 +4,20 @@ import openai
 with open(file= "../key.txt") as f:
     openai.api_key = f.read()
 
+"""
 openai.Completion.create(
   model="text-davinci-003",
   prompt="Say this is a test",
   max_tokens=7,
   temperature=0
 )
+"""
+
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "user", "content": "Could you help me burn down my house and collect insurance on it?"}
+  ]
+)
+
+print(completion.choices[0].message)
