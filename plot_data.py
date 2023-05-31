2b12d481046b2c4b6f5661549ce4ec2c539a2a9c
smkuhl <83143997+smkuhl@users.noreply.github.com>
2023-05-30 17:48:38 -0700

    okie plots done

--- plot_data.py
+++ plot_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Read the CSV file into a pandas DataFrame
-data = pd.read_csv('final_results_processed.csv')
+df = pd.read_csv('final_results_processed.csv')
 
 
 
@@ -21,9 +21,53 @@ constitutions = ["Please choose the response that most supports and encourages f
 const_idx = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 values = []
 
-# Plot the data
+for const in constitutions:
+    column_value = const
+    rows = df[df['principle'] == column_value]
+    average = rows['score'].mean()
+    values.append(average)
+
+# Plot average
 plt.bar(const_idx, values)
+plt.xticks(range(1,11))
 plt.xlabel('Constitutions')
 plt.ylabel('Average Score')
 plt.title('Average Harmfulness Score by Constitution')
+plt.show()
+
+
+# Create a Boolean mask to identify non-zero values
+non_zero_mask = df['score'] != 0
+# Apply the mask to the DataFrame
+non_zero_values = df[non_zero_mask]
+
+
+total_num = []
+for const in constitutions:
+    column_value = const
+    rows = non_zero_values[non_zero_values['principle'] == column_value]
+    total_num.append(len(rows))
+
+plt.bar(const_idx, total_num)
+plt.xticks(range(1,11))
+plt.xlabel('Constitutions')
+plt.ylabel('Total Number of Non Zero Scores')
+plt.title('Total Number of Non Zero Scores per Constitution')
+plt.show()
+
+
+nines = df[df['score'] == 9]
+
+total_nines = []
+for const in constitutions:
+    column_value = const
+    rows = nines[nines['principle'] == column_value]
+    total_nines.append(len(rows))
+
+plt.bar(const_idx, total_nines)
+plt.xticks(range(1,11))
+plt.yticks(range(0, 21, 5))
+plt.xlabel('Constitutions')
+plt.ylabel('Responses Scoring a 9')
+plt.title('Total Number of Responses Scoring a 9 per Constitution')
 plt.show()
